778176cc994f086c1471dbc91527fd9b956a4101
Udit Raj <udit.r@simformsolutions.com>
2023-04-28 10:03:50 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:chapter_5/pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Chapter 5",
+      home: Home(),
+    );
+  }
+}
